d2102878a0fcf1b3b09d9754ed55248da056d6b8
Kevin <k.janssen11@student.avans.nl>
2025-06-14 12:58:22 +0200

    layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/menukaart', [MenuController::class, 'index']);
