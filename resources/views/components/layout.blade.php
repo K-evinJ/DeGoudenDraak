d1245baf6719b07cfac58e05bdbe22a3b3b0ba8c
Kevin <k.janssen11@student.avans.nl>
2025-06-14 16:54:11 +0200

    header

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>The Golden Dragon</title>
+    @vite('resources/css/app.css')
+
+    @yield('styles')
+    @yield('scripts')
 </head>
-<body>
+<body class="h-screen w-screen">
     {{ $slot }}
 </body>
 </html>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,3 +1,15 @@
 <x-layout>
-    <h1>Test</h1>
+
+    @section('styles')
+        <link rel="stylesheet" href="{{ asset('css/menu.css') }}">
+    @endsection
+
+    <div class="bg-[#ff0000] mx-[50px] my-[15px]">
+        <header class="flex justify-evenly items-center">
+            <x-header-logo></x-header-logo>
+            <a href="#" class="text-[yellow] font-bold">Welkom bij De Gouden Draak. Klik op deze tekst om de aanbiedingen van deze week te zien!</a>
+            <x-header-logo></x-header-logo>
+        </header>
+
+    </div>
 </x-layout>
