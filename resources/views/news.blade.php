d99f5a6b74a7e6b4324c35a78f04f871b3fad5bf
Kevin <k.janssen11@student.avans.nl>
2025-06-15 21:05:54 +0200

    text warp balance

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <main class="flex justify-center w-full min-h-30 bg-amber-50  border-1"> 
-        <p class="self-center w-full text-center font-bold font-[chinese] text-xl">{{ $news }}</p>
+    <main class="flex justify-center w-full min-h-30 bg-[floralwhite]  border-1"> 
+        <p class="self-center w-full text-center font-bold font-[chinese] text-[18px] [text-wrap:balance]">{{ $news }}</p>
     </main>
     <div class="w-[50px]"></div>
 </x-layout>
