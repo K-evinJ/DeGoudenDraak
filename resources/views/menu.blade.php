93fa621060b16369f94d545cba78f4d259b51ae8
Kevin <k.janssen11@student.avans.nl>
2025-06-19 22:35:50 +0200

    flow

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <main class="flex flex-col gap-1 text-white font-[chinese] text-[20px]">
-        <a href="{{ route('download.menu') }}" class="bg-[url(/public/images/menu_bg_gradient.png)] border-r-1 border-b-1 border-white hover:cursor-pointer h-7 w-fit px-3 text-center self-end">
-            Download Menu
+        <a href="{{ route('dishes') }}" class="bg-[url(/public/images/menu_bg_gradient.png)] border-r-1 border-b-1 border-white hover:cursor-pointer h-7 w-fit px-3 text-center self-end">
+            Actueel Menu
         </a>
         <div class="self-center border-1">
             <img src="{{ asset('images/restaurant-menukaart-1-2.jpg') }}" alt="menu1">
