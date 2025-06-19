5402f85b23b870d00cbed5ce5fe24ed358c6acb5
Kevin <k.janssen11@student.avans.nl>
2025-06-19 15:47:25 +0200

    download

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Carbon;
 use App\Models\Discount;
 
 use function Spatie\LaravelPdf\Support\pdf;
+use Spatie\LaravelPdf\Facades\Pdf;
 use Spatie\LaravelPdf\Enums\Format;
 
 class MenuController extends Controller
@@ -38,6 +39,6 @@ class MenuController extends Controller
     {
         $dishTypes = Dish::where('visible', 1)->orderBy('number', 'asc')->get()->groupby('dish_type');
         $discounts = Discount::where('end_date', '>', Carbon::now())->with('dish')->get();
-        return pdf('menu-pdf', ['dishTypes' => $dishTypes, 'discounts' => $discounts])->landscape();
+        return Pdf::view('menu-pdf', ['dishTypes' => $dishTypes, 'discounts' => $discounts])->landscape()->download('menukaart.pdf');
     }
 }
